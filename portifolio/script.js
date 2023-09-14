3f2e2ca4ca9388ad60de46d039b805d6ef2550a7
Ravi Kumar <132846098+Ravikumar1908@users.noreply.github.com>
2023-09-14 15:51:46 +0530

    Add files via upload

---
+++ portifolio/script.js
@@ -0,0 +1,18 @@
+function socialshow()
+{
+    var a=document.getElementById("social");
+    if(a.style.display==="none")
+    {
+        a.style.display="block";
+    }
+    else{
+        a.style.display="none";
+    }
+}
+
+
+let a={
+    name:'ravi',
+    rollNumber:21,
+}
+document.write(a.name);
